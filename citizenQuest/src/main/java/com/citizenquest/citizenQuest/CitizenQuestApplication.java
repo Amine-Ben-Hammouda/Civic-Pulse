56d54c15d68fb4dabb6b05d4a804871735fe66c3
Amine-Ben-Hammouda <bhamine1996@gmail.com>
2026-02-28 15:20:10 +0100

    project initialization

---
+++ citizenQuest/src/main/java/com/citizenquest/citizenQuest/CitizenQuestApplication.java
@@ -0,0 +1,13 @@
+package com.citizenquest.citizenQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenQuestApplication.class, args);
+	}
+
+}
---
+++ citizenQuest/src/test/java/com/citizenquest/citizenQuest/CitizenQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizenquest.citizenQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
